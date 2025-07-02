092e22844d9a16defc7f22d150437fe5bad7a477
AlbertoManish1234 <manish.student.che24@itbhu.ac.in>
2025-07-02 15:13:11 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId="1222221"
+let accountEmail="abs@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+accountEmail="ababab@iitd.ac.in"
+accountPassword="1212121212"
+accountCity="Delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
